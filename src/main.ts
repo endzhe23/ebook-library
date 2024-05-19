8d61fb347caf7bef0c6ef3437ff63d8eee49e4da
endzhe23 <endzhe.yarullina@gmail.com>
2024-05-19 20:21:51 +0300

    add global prefix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { setupSwagger } from './swagger/api.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
+  app.setGlobalPrefix('api/v1');
   setupSwagger(app);
   await app.listen(3000);
 }
